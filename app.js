5cfc56f49e4e61b2caf8f3a1bc93b9b540e0c207
vipin14444 <vipin.sharma3039@gmail.com>
2021-09-11 10:32:52 +0530

    Added utils

--- app.js
+++ app.js
@@ -10,6 +10,45 @@ const getName = (element) => {
     return $("span.jcn > a > span").text() || "";
 };
 
+const getPhoneNumber = (element) => {
+    const map = {
+        "icon-ji": 9,
+        "icon-dc": "+",
+        "icon-fe": "(",
+        "icon-hg": ")",
+        "icon-ba": "-",
+        "icon-lk": 8,
+        "icon-nm": 7,
+        "icon-po": 6,
+        "icon-rq": 5,
+        "icon-ts": 4,
+        "icon-vu": 3,
+        "icon-wx": 2,
+        "icon-yz": 1,
+        "icon-acb": 0,
+    };
+
+    let $ = cheerio.load(element);
+
+    let phoneNumber = "";
+
+    $("p.contact-info > span span").each((i, item) => {
+        const className = item.attribs.class;
+        for (let cls in map) {
+            if (className.includes(cls)) {
+                phoneNumber += map[cls];
+            }
+        }
+    });
+
+    return phoneNumber;
+};
+
+const getAddress = (element) => {
+    let $ = cheerio.load(element);
+    return $("span.mrehover > .cont_fl_addr").text() || "";
+};
+
 (async () => {
     let pageNumber = 1;
 
@@ -23,6 +62,9 @@ const getName = (element) => {
         $("li.cntanr").each((i, element) => {
             let data = {};
             data.name = getName(element);
+            data.phoneNumber = getPhoneNumber(element);
+            data.address = getAddress(element);
+            console.log(data);
         });
         ++pageNumber;
     }
